f06bd293800a5a166ff82766a9724f7ecf3669ff
ilya-rikow <62751670+ilya-rikow@users.noreply.github.com>
2020-04-19 17:34:02 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$('.header__burger').click(function(event){
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+const authButton = document.querySelector("#auth-button");
+const modalAuth = document.querySelector(".modal-auth");
+const closeAuth = document.querySelector(".close-auth");
+const authmobButton = document.querySelector("#auth-button-mob");
+const details = document.querySelector("#det");
+const modalDet = document.querySelector(".modal-details");
+const closeDet = document.querySelector(".close-details");
+
+
+
+
+
+authButton.addEventListener('click', toggleModalAuth);
+closeAuth.addEventListener('click', toggleModalAuth);
+
+
+function toggleModalAuth(){
+	modalAuth.classList.toggle("is-open-auth");
+};
+authmobButton.addEventListener('click', toggleModalAuth);
+closeAuth.addEventListener('click', toggleModalAuth);
+
+
+function toggleModalAuth(){
+	modalAuth.classList.toggle("is-open-auth");
+};
+
+details.addEventListener('click', toggleModalDet);
+closeDet.addEventListener('click', toggleModalDet);
+
+
+function toggleModalDet(){
+	modalDet.classList.toggle("is-open");
+	$('body').toggleClass('lock');
+};
+
+
+
+
